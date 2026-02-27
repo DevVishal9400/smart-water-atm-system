f6425622d5a6c1522916bd6a6bf8c3d162a2eb3e
DevVishal9400 <vishaltilekar7772@gmail.com>
2026-02-27 11:30:52 +0530

    added some security related stuff

--- src/main/java/com/vt/water/atm/security/config/SecurityConfig.java
+++ src/main/java/com/vt/water/atm/security/config/SecurityConfig.java
@@ -1,4 +1,41 @@
 package com.vt.water.atm.security.config;
 
+import com.vt.water.atm.security.jwt.JWTAuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private JWTAuthFilter jwtAuthFilter;
+    //securityfilter chain
+    @Bean
+    public SecurityFilterChain getFilterChain(HttpSecurity httpSecurity) {
+
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
+
+        return httpSecurity.build();
+
+
+    }
+
+    @Bean
+    public PasswordEncoder getPassEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
